9baf51fd4afd06d1a20257d31e6590e7ba9a77bf
Peroy Nel <peroynel@yahoo.com>
2022-02-17 16:26:35 +0200

    PopularItems Component Implemented, Styled and ScrollView Implemented
    PopularItems Component Implemented, Styled and ScrollView Implemented

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Image, Text, StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import { View, Image, Text, StyleSheet, SafeAreaView, FlatList, ScrollView } from 'react-native';
 import categoriesData from '../assets/data/categoriesData';
 import popularData from '../assets/data/popularData';
 
@@ -30,37 +30,77 @@ export default Home = () => {
 
   return (
     <View style={styles.container}>
-        <SafeAreaView>
-            <View style={styles.headerWrapper}>
-                <Image source={require('../assets/images/profile.png')} style={styles.profileImage}/>
-                <Feather name="menu" size={24} color={colors.textDark}/>
-            </View>
-        </SafeAreaView>
+        <ScrollView>
+                <SafeAreaView>
+                <View style={styles.headerWrapper}>
+                    <Image source={require('../assets/images/profile.png')} style={styles.profileImage}/>
+                    <Feather name="menu" size={24} color={colors.textDark}/>
+                </View>
+            </SafeAreaView>
 
-        {/* Titles */}
+            {/* Titles */}
 
-        <View style={styles.titleWrapper}>
-            <Text style={styles.titleSubtitle}>Food</Text>
-            <Text style={styles.titleTitle}>Delivery</Text>
-        </View>
-
-        {/* Search */}
-        <View style={styles.searchWrapper}>
-            <Feather name="search" size={16} color={colors.textDark}/>
-            <View style={styles.search}>
-                <Text style={styles.searchText}>Search</Text>
-            </View>
-        </View>
-
-        {/* Categories */}
-
-        <View style={styles.categoriesWrapper}>
-            <Text style={styles.categoriesTitle}>Categories</Text>
-            <View style={styles.categoriesListWrapper}>
-                <FlatList data={categoriesData} renderItem={renderCategoryItem} keyExtractor={(item) => item.id} horizontal={true}/>
+            <View style={styles.titleWrapper}>
+                <Text style={styles.titleSubtitle}>Food</Text>
+                <Text style={styles.titleTitle}>Delivery</Text>
             </View>
 
-        </View>
+            {/* Search */}
+            <View style={styles.searchWrapper}>
+                <Feather name="search" size={16} color={colors.textDark}/>
+                <View style={styles.search}>
+                    <Text style={styles.searchText}>Search</Text>
+                </View>
+            </View>
+
+            {/* Categories */}
+
+            <View style={styles.categoriesWrapper}>
+                <Text style={styles.categoriesTitle}>Categories</Text>
+                <View style={styles.categoriesListWrapper}>
+                    <FlatList data={categoriesData} renderItem={renderCategoryItem} keyExtractor={(item) => item.id} horizontal={true}/>
+                </View>
+
+            </View>
+
+            <View style={styles.popularWrapper}>
+                <Text style={styles.popularTitle}>Popular</Text>
+                {popularData.map(item => (
+                    <View style={[styles.popularCardWrapper, 
+                        {
+                        marginTop: item.id == 1 ? 10 : 20,
+                        },
+                    ]}>
+                        <View>
+                            <View>
+                                <View style={styles.popularTopWrapper}>
+                                    <MaterialCommunityIcons name="crown" size={12} color={colors.primary} />
+                                    <Text style={styles.popularTopText}>Top of the week</Text>
+                                </View>
+                                <View style={styles.popularTitlesWrapper}>
+                                    <Text style={styles.popularTitlesTitle}>{item.title}</Text>
+                                    <Text style={styles.popularTitlesWeight}>Weight: {item.weight}</Text>
+                                </View>
+                            </View>
+                            <View style={styles.popularCardBottom}>
+                                <View style={styles.addPizzaButton}>
+                                    <Feather name="plus" size={10}/>
+
+                                </View>
+                                <View style={styles.ratingWrapper}>
+                                    <MaterialCommunityIcons name="star" size={10} color={colors.textDark} />
+                                    <Text style={styles.rating}>{item.rating}</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={styles.popularCardRight}>
+                            <Image source={item.image} style={styles.popularCardImage} />
+                        </View>
+                    </View>
+                ))}
+
+            </View>
+        </ScrollView>
 
     </View>
   )
@@ -174,5 +214,84 @@ const styles = StyleSheet.create({
     categoriesSelectIcon: {
         alignSelf: 'center',
 
-    },   
+    },
+    popularWrapper: {
+        paddingHorizontal: 20,
+    },
+    popularTitle: {
+        fontFamily: 'Montserrat-Bold',
+        fontSize: 16,
+
+    },
+    popularCardWrapper: {
+        backgroundColor: colors.white,
+        borderRadius: 25,
+        paddingTop: 20,
+        paddingLeft: 20,
+        flexDirection: 'row',
+        overflow: 'hidden',
+
+    },
+    popularTopWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+
+    },
+    popularTopText: {
+        marginLeft: 10,
+        fontFamily: 'Montserrat-Bold',
+        fontSize: 14,
+
+    },
+    popularTitlesWrapper: {
+        marginTop: 20,
+        
+    },
+    popularTitlesTitle: {
+        fontFamily: 'Montserrat-SemiBold',
+        fontSize: 14,
+        color: colors.textDark,
+    },
+    popularTitlesWeight: {
+        fontFamily: 'Montserrat-Medium',
+        fontSize: 12,
+        color: colors.textLight,
+    },
+    popularCardBottom: {
+        flexDirection: 'row',
+        alignItem: 'center',
+        marginTop: 10,
+        marginLeft: -20,
+
+    },
+    addPizzaButton: {
+        backgroundColor: colors.primary,
+        paddingHorizontal: 40,
+        paddingVertical: 20,
+        borderTopRightRadius: 25,
+        borderBottomLeftRadius: 25,
+
+    },
+    ratingWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 20,
+    },
+    rating: {
+        fontFamily: 'Montserrat-SemiBold',
+        fontSize: 12,
+        color: colors.textDark,
+        marginLeft: 5,
+
+    },
+    popularCardRight: {
+        marginLeft: 20,
+
+    },
+    popularCardImage: {
+        width: 210,
+        height: 125,
+        resizeMode: 'contain',
+
+    },      
   });
